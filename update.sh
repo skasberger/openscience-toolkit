9d3448cda4bdec4e7e1f3d20150428e5db5eb71e
Stefan Kasberger <stefan.kasberger@alpine-geckos.at>
2013-07-27 16:40:40 +0200

    fix: changed folder from .openscience-script to openscience-script

--- update.sh
+++ update.sh
@@ -6,69 +6,69 @@
 # Description: Updates all downloaded files to the actual status
 #
 
-cd ~/.openscience-scripts/
+cd ~/openscience-scripts/
 
 # download zsh config by grml
-if [ ! -d ~/.openscience-scripts/shell/configs/grml]; then
-	mkdir ~/.openscience-scripts/ext/zsh-config-grml
+if [ ! -d ~/openscience-scripts/shell/configs/grml]; then
+	mkdir ~/openscience-scripts/ext/zsh-config-grml
 fi
-cd ~/.openscience-scripts/ext/zsh-config-grml
+cd ~/openscience-scripts/ext/zsh-config-grml
 wget -O .zshrc http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 wget -O .zshrc.local  http://git.grml.org/f/grml-etc-core/etc/skel/.zshrc
-cd ~/.openscience-scripts/
+cd ~/openscience-scripts/
 
 # download bash and zsh config by Stefan Kasberger
 
 
 # download file templates by Stefan Kasberger (only code & licenses)
-mkdir ~/.openscience-scripts/ext/templates-skasberger
-mkdir ~/.openscience-scripts/.tmp
-cd ~/.openscience-scripts/.tmp
+mkdir ~/openscience-scripts/ext/templates-skasberger
+mkdir ~/openscience-scripts/.tmp
+cd ~/openscience-scripts/.tmp
 wget https://github.com/skasberger/templates/archive/master.zip
 unzip master.zip
-mv ~/.openscience-scripts/.tmp/templates-master/code/ ~/.openscience-scripts/ext/templates-skasberger
-mv ~/.openscience-scripts/.tmp/templates-master/licenses ~/.openscience-scripts/ext/templates-skasberger
-cd ~/.openscience-scripts/
+mv ~/openscience-scripts/.tmp/templates-master/code/ ~/openscience-scripts/ext/templates-skasberger
+mv ~/openscience-scripts/.tmp/templates-master/licenses ~/openscience-scripts/ext/templates-skasberger
+cd ~/openscience-scripts/
 rm -R .tmp
 
 # download gitignore files by github
-mkdir ~/.openscience-scripts/ext/gitignore-github
-mkdir ~/.openscience-scripts/.tmp
-cd ~/.openscience-scripts/.tmp
+mkdir ~/openscience-scripts/ext/gitignore-github
+mkdir ~/openscience-scripts/.tmp
+cd ~/openscience-scripts/.tmp
 wget https://github.com/github/gitignore/archive/master.zip
 unzip master.zip
-mv ~/.openscience-scripts/.tmp/gitignore-master/* ~/.openscience-scripts/ext/gitignore-github
-cd ~/.openscience-scripts/
+mv ~/openscience-scripts/.tmp/gitignore-master/* ~/openscience-scripts/ext/gitignore-github
+cd ~/openscience-scripts/
 rm -R .tmp
 
 # download bookmarks2mediawiki by Stefan Kasberger
-mkdir ~/.openscience-scripts/ext/bookmarks2mediawiki-skasberger
-mkdir ~/.openscience-scripts/.tmp
-cd ~/.openscience-scripts/.tmp
+mkdir ~/openscience-scripts/ext/bookmarks2mediawiki-skasberger
+mkdir ~/openscience-scripts/.tmp
+cd ~/openscience-scripts/.tmp
 wget https://github.com/skasberger/bookmarks2mediawiki/archive/master.zip
 unzip master.zip
-mv ~/.openscience-scripts/.tmp/bookmarks2mediawiki-master/* ~/.openscience-scripts/ext/bookmarks2mediawiki-skasberger
-cd ~/.openscience-scripts/
+mv ~/openscience-scripts/.tmp/bookmarks2mediawiki-master/* ~/openscience-scripts/ext/bookmarks2mediawiki-skasberger
+cd ~/openscience-scripts/
 rm -R .tmp
 
 # download licenses by OKFN
-mkdir ~/.openscience-scripts/ext/licenses-okfn
-mkdir ~/.openscience-scripts/.tmp
-cd ~/.openscience-scripts/.tmp
+mkdir ~/openscience-scripts/ext/licenses-okfn
+mkdir ~/openscience-scripts/.tmp
+cd ~/openscience-scripts/.tmp
 wget https://github.com/okfn/licenses/archive/master.zip
 unzip master.zip
-mv ~/.openscience-scripts/.tmp/licenses-master/* ~/.openscience-scripts/ext/licenses-okfn
-cd ~/.openscience-scripts/
+mv ~/openscience-scripts/.tmp/licenses-master/* ~/openscience-scripts/ext/licenses-okfn
+cd ~/openscience-scripts/
 rm -R .tmp
 
 # download templates by openscienceASAP
-mkdir ~/.openscience-scripts/ext/templates-openscienceASAP
-mkdir ~/.openscience-scripts/.tmp
-cd ~/.openscience-scripts/.tmp
+mkdir ~/openscience-scripts/ext/templates-openscienceASAP
+mkdir ~/openscience-scripts/.tmp
+cd ~/openscience-scripts/.tmp
 wget https://github.com/openscienceASAP/templates/archive/master.zip
 unzip master.zip
-mv ~/.openscience-scripts/.tmp/templates-master/* ~/.openscience-scripts/ext/templates-openscienceASAP
-cd ~/.openscience-scripts/
+mv ~/openscience-scripts/.tmp/templates-master/* ~/openscience-scripts/ext/templates-openscienceASAP
+cd ~/openscience-scripts/
 rm -R .tmp
 
 
